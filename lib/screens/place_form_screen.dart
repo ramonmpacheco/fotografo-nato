27eaa43a69a9f642855e2d616a5a5c4d97740939
Ramon Pacheco <rmpacheco2@gmail.com>
2021-08-15 11:55:51 -0300

    add:  place form - wip

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fotografo_nato/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   @override
@@ -6,14 +7,49 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Novo Lugar'),
       ),
-      body: Center(
-        child: Text('Form!!'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    ImageInput()
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            onPressed: _submitForm,
+            icon: Icon(Icons.add),
+            label: Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).accentColor,
+                elevation: 0,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                padding: EdgeInsets.all(20)),
+          )
+        ],
       ),
     );
   }
