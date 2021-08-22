87fdb8c9aeaac72ffbd8612a48f5e45da3ea5e0e
Ramon Pacheco <rmpacheco2@gmail.com>
2021-08-22 17:02:38 -0300

    add: map screen

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fotografo_nato/screens/map_screen.dart';
+import 'package:fotografo_nato/utils/location_util.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   @override
@@ -7,6 +10,26 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locationData = await Location().getLocation();
+    final staticMapImageUrl = LocationUtil.generateLocationPreviewImage(
+      latitude: locationData.latitude,
+      longitude: locationData.longitude,
+    );
+    print(staticMapImageUrl);
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  Future<void> _selectOnMap() async {
+    final selectedLocation = await Navigator.of(context)
+        .push(MaterialPageRoute(builder: (ctx) => MapScreen()));
+
+    if (selectedLocation == null) return;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -30,12 +53,12 @@ class _LocationInputState extends State<LocationInput> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentUserLocation,
               icon: Icon(Icons.location_on),
               label: Text('Localização Atual'),
             ),
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _selectOnMap,
               icon: Icon(Icons.map),
               label: Text('Selecione no mapa'),
             )
